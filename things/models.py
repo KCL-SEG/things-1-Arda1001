77868dfd089a8ed28f07f404c5c6313f10e40448
Arda Ordu <ardaordu03@gmail.com>
2022-10-09 23:29:54 +0100

    Fix description constraint

--- things/models.py
+++ things/models.py
@@ -4,5 +4,5 @@ from django.core.validators import MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length = 30, unique = True, blank = False,)
-    description = models.CharField(max_length = 120, unique = False,)
+    description = models.CharField(max_length = 120, unique = False, blank = True,)
     quantity = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(100)],)
